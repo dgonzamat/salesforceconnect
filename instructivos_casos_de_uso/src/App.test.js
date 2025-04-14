b453c2f22d4734c6dbca2a0020cb13aee752d0c5
dgonzamat <133278940+dgonzamat@users.noreply.github.com>
2025-04-14 13:44:35 -0400

    Delete instructivos_casos_de_uso directory

--- instructivos_casos_de_uso/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the header with correct title', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/Instructivos Casos de Uso/i);
-  expect(headerElement).toBeInTheDocument();
-});
